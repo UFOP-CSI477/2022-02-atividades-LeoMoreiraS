ca6709e7db1133b3d031d48d1bec6bc618a31ce9
leonardo.sepulveda <leonardo.sepulveda@telefonicabigdata.onmicrosoft.com>
2023-03-10 21:30:55 -0300

    feat: add graphql typescript prisma integration

--- Atividades/atividade-pratica-01/server/src/app.ts
+++ Atividades/atividade-pratica-01/server/src/app.ts
@@ -1,9 +1,36 @@
+import 'reflect-metadata'
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { resolvers } from "@generated/type-graphql";
 import dotenv from "dotenv";
 const app = express();
+import { buildSchema } from "type-graphql";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+const schema = await buildSchema({
+  resolvers,
+  validate: false,
+});
 dotenv.config();
+
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+
+app.use('/graphql', graphqlHTTP({
+  schema:  schema,
+  rootValue: root,
+  graphiql: true,
+  context: {prisma: prisma}
+}));
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at ${process.env.PORT}`);
 });
+
