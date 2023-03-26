e95fb247bb5de97bd9c2755938472c729377b72d
leonardo.sepulveda <leonardo.sepulveda@telefonicabigdata.onmicrosoft.com>
2023-03-26 12:01:38 -0300

    rename estados to pages

--- Atividades/atividade-pratica-02/web/src/routes.tsx
+++ Atividades/atividade-pratica-02/web/src/routes.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
-import BloodTypes from './components/estados/BloodTypes';
-import Cities from './components/estados/Cities';
-import CollectionLocations from './components/estados/CollectionLocations';
-import Donations from './components/estados/Donation';
-import Peoples from './components/estados/People';
-import States from './components/estados/States';
+import BloodTypes from './components/pages/BloodTypes';
+import Cities from './components/pages/Cities';
+import CollectionLocations from './components/pages/CollectionLocations';
+import Donations from './components/pages/Donation';
+import Peoples from './components/pages/People';
+import States from './components/pages/States';
 
 
 const AppRoutes = () => {
